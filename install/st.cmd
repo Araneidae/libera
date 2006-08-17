e96de853805d9e8116e63b551cefa0fcbeba6cd5
mga83 <mga83@e099a375-04f9-0310-9d5f-a741eaff62e1>
2006-08-17 12:10:17 +0000

    Libera:  Installation and configuration changes.  - Add doupgrade to toolkit  - Simplify CONFIG management by removing _TARGET definitions.  These are now    computed in the libera-install-ioc script.
    git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/diamond/trunk/ioc/Libera@3738 e099a375-04f9-0310-9d5f-a741eaff62e1

--- install/st.cmd
+++ install/st.cmd
@@ -1,8 +1,5 @@
 # IOC startup script for Libera EPICS driver.
-
 dbLoadDatabase("dbd/ioc.dbd",0,0)
 ioc_registerRecordDeviceDriver(pdbbase)
-
 dbLoadRecords("db/libera.db", "${LIBERA_MACROS}")
-
 iocInit()
