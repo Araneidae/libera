c979778d74fe54319d133d506f46f388cce0a6cc
mga83 <mga83@e099a375-04f9-0310-9d5f-a741eaff62e1>
2009-05-18 06:52:33 +0000

    Libera: Modify strip tool display buttons (SA and SC) to use purely proportional ranges.  Also unify the two nearly identical launcher scripts channels.py and buttons.py into a common strip_tool.py script.
    git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/diamond/trunk/ioc/Libera@31775 e099a375-04f9-0310-9d5f-a741eaff62e1

--- opi/scripts/strip_tool.py
+++ opi/scripts/strip_tool.py
@@ -4,20 +4,30 @@
 import sys, os, subprocess
 import tempfile
 from pkg_resources import require
-require("dls.ca2")
-from dls.ca2 import catools
+require('cothread')
+from cothread import catools
 
 
-if len(sys.argv) != 2:
-    print "usage: channels.py <bpm>"
+try:
+    pattern, channels, window_size, interval, BPM = sys.argv[1:]
+    window_size = float(window_size)
+    interval = float(interval)
+except:
+    print \
+'''usage: strip_tool.py <pattern> <channels> <window> <interval> <bpm>
+
+Examples:
+
+    strip_tool.py SA:%s     ABCD 0.01  0.1 <bpm>
+    strip_tool.py SC:C%sMAG 1234 0.001 1   <bpm>
+'''
     sys.exit(0);
 
     
-BPM = sys.argv[1]
-Channels = ['%s:SC:C%sMAG' % (BPM, channel) for channel in '1234']
+Channels = ['%s:%s' % (BPM, pattern % channel) for channel in channels]
 
 # Read the current values of the channels
-ChannelValues = [c.value for c in catools.caget(Channels, timeout=5)]
+ChannelValues = catools.caget(Channels, timeout=5)
 
 # Create a temporary configuration file.
 config = tempfile.NamedTemporaryFile(prefix='%s.' % BPM)
@@ -26,18 +36,17 @@ config = tempfile.NamedTemporaryFile(prefix='%s.' % BPM)
 config.write('''StripConfig                   1.2
 Strip.Time.Timespan           300
 Strip.Time.NumSamples         65536
-Strip.Time.SampleInterval     1
-Strip.Time.RefreshInterval    1
+Strip.Time.SampleInterval     %(interval)g
+Strip.Time.RefreshInterval    %(interval)g
 Strip.Option.GraphLineWidth   0
-''')
+''' % locals())
 
 # Write our field specific bits.  We zoom in to +-1% of the initial value of
 # each button.
-window_size = 0.001
 for curve, (ChannelName, ChannelValue) in \
         enumerate(zip(Channels, ChannelValues)):
-    min_value = ChannelValue - window_size
-    max_value = ChannelValue + window_size
+    min_value = ChannelValue * (1 - window_size)
+    max_value = ChannelValue * (1 + window_size)
     config.write('''Strip.Curve.%(curve)d.Name     %(ChannelName)s
 Strip.Curve.%(curve)d.Units    mm
 Strip.Curve.%(curve)d.Min      %(min_value)g
@@ -48,13 +57,13 @@ config.flush()
 
 # run StripTool with our config file
 try:
-    retcode = subprocess.call("StripTool " + config.name, shell=True)
+    retcode = subprocess.call('StripTool ' + config.name, shell=True)
     if retcode < 0:
-        print >>sys.stderr, "StripTool was terminated by signal", -retcode
+        print >>sys.stderr, 'StripTool was terminated by signal', -retcode
     else:
-        print >>sys.stderr, "StripTool returned", retcode
+        print >>sys.stderr, 'StripTool returned', retcode
 except OSError, e:
-    print >>sys.stderr, "Execution failed:", e
+    print >>sys.stderr, 'Execution failed:', e
     
 # remove the temporary file
 config.close()
